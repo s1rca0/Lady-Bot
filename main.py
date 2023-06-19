bf5f5ada3c916122c873a8a436f24385a5906c1b
s1rca0 <136553233+s1rca0@users.noreply.github.com>
2023-06-19 02:31:40 +0000

    Added slash commands

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 import os
 token = os.environ['DISCORD_TOKEN']
@@ -8,7 +9,20 @@ bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print("Bot is Online") # bot status msg
-    
+  print("Bot is Online") # bot status msg
+  try:
+    synced = await bot.tree.sync()
+    print(f"Synced {len(synced)} command(s)")
+  except Exception as e:
+    print(e)
+
+@bot.tree.command(name="hello")
+async def hello(interaction: discord.Interaction):
+  await interaction.response.send_message(f"Hey, {interaction.user.mention}! This is a slash command", ephemeral=True)
+
+@bot.tree.command(name="say")
+@app_commands.describe(thing_to_say = "What should I say?")
+async def say(interaction: discord.Interaction, thing_to_say: str):
+  await interaction.response.send_message(f"{interaction.user.name} said: `{thing_to_say}`")
 
 bot.run(token)
