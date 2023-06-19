b9ba4ee934fe1666acf8939c9472a13077c3a728
s1rca0 <136553233+s1rca0@users.noreply.github.com>
2023-06-19 01:19:15 +0000

    Added #notes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import os
 token = os.environ['DISCORD_TOKEN']
 
+# Adds prefix and intents
 bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
 @bot.event
